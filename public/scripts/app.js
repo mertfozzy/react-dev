5f255d598b558aadaaedba053a035b9c7804cf4f
Mert Altuntaş <mertfozzy@gmail.com>
2021-09-13 20:21:21 +0300

    13.12.2021

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,22 @@
 "use strict";
 
 var root = document.getElementById("root");
-var template = /*#__PURE__*/React.createElement("h1", {
+var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", {
   id: "header"
-}, "Hello World Nigga!");
+}, "Hello World Nigga!"), /*#__PURE__*/React.createElement("div", null, "Lorem, ipsum dolor."), /*#__PURE__*/React.createElement("ul", null, /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum."), /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum."), /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum.")));
+var product = {
+  name: "iPhone 8",
+  price: 3000,
+  description: "a nice budget phone"
+};
+var template2 = /*#__PURE__*/React.createElement("div", {
+  id: "product-details"
+}, /*#__PURE__*/React.createElement("h2", {
+  id: "product-name"
+}, "name : ", product.name), /*#__PURE__*/React.createElement("p", {
+  id: "product-price"
+}, "price : ", product.price, "\u20BA"), /*#__PURE__*/React.createElement("p", {
+  id: "product-description"
+}, "description : ", product.description));
 ReactDOM.render(template, root);
+ReactDOM.render(template2, root);
--- src/app.js
+++ src/app.js
@@ -1,5 +1,35 @@
 var root = document.getElementById("root");
 
-var template = <h1 id="header">Hello World Nigga!</h1>;
+var template = 
+<div>
+<h1 id="header">Hello World Nigga!</h1>
+<div>Lorem, ipsum dolor.</div>
+
+<ul>
+    <li>Lorem, ipsum.</li>
+    <li>Lorem, ipsum.</li>
+    <li>Lorem, ipsum.</li>
+</ul>
+
+</div>;
+
+
+
+var product = {
+    name :  "iPhone 8",
+    price : 3000,
+    description : "a nice budget phone"
+};
+
+
+var template2 = <div id="product-details">
+
+<h2 id="product-name">name : {product.name}</h2>
+<p id="product-price">price : {product.price}₺</p>
+<p id="product-description">description : {product.description}</p>
+
+</div>;
+
 
 ReactDOM.render(template, root);
+ReactDOM.render(template2, root);
