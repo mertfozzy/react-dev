1e2fce007b8fdcd5f5e62cec27c3c78b6022751a
Mert Altuntaş <mertfozzy@gmail.com>
2021-09-13 18:34:38 +0300

    13.12.2021

---
+++ public/scripts/app.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var root = document.getElementById("root");
+var template = /*#__PURE__*/React.createElement("h1", {
+  id: "header"
+}, "Hello World Nigga!");
+ReactDOM.render(template, root);
---
+++ src/app.js
@@ -0,0 +1,5 @@
+var root = document.getElementById("root");
+
+var template = <h1 id="header">Hello World Nigga!</h1>;
+
+ReactDOM.render(template, root);
